5646bc824c699b7b370781e01568589b1f7368fd
Ana <agdr20@gmail.com>
2021-03-16 22:06:33 +0000

    Add home view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,18 @@
 import Head from 'next/head';
 
 import { HEAD_TITLE } from '@constants/seo';
+import Home from '@views/Home';
 
-function HomePage(): JSX.Element {
+const HomePage = (): JSX.Element => {
   return (
     <>
       <Head>
         <title>{HEAD_TITLE}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex items-center justify-center min-h-screen text-6xl bg-purple-200">
-        Hi, I&apos;m Ana!
-      </div>
+      <Home />
     </>
   );
-}
+};
 
 export default HomePage;
---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,13 @@
+import Layout from '@layout';
+
+const Home = (): JSX.Element => (
+  <Layout>
+    <Layout.Content>
+      <div className="flex items-center justify-center min-h-screen text-6xl bg-purple-200">
+        Hi, I&apos;m Ana! 🐈
+      </div>
+    </Layout.Content>
+  </Layout>
+);
+
+export default Home;
