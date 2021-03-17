6d2be4d944833ffb186ace6efe3640327101a947
Ana <agdr20@gmail.com>
2021-03-17 22:26:04 +0000

    Add some colors

--- src/views/Home/Hero/Hero.tsx
+++ src/views/Home/Hero/Hero.tsx
@@ -1,22 +1,22 @@
 const Hero = (): JSX.Element => {
   return (
-    <section className="flex flex-col items-center justify-center min-h-screen bg-purple-500">
-      <div className="container">
+    <section className="flex flex-col items-center justify-center min-h-screen bg-purple-darkest">
+      <div className="container text-gray-300">
         <h1 className="flex flex-col">
           <span>Hi, my name is</span>
-          <span>Ana! 🐈</span>
+          <span className="text-3xl text-orange-400">Ana</span>
         </h1>
         <div>
-          <h2>I solve problems with code.</h2>
+          <h2 className="font-mono text-3xl">I solve problems with code.</h2>
         </div>
-        <div>
+        <div className="text-gray-400">
           <p>
-            I&apos;m a software engineer, with a bioinformatics background,
-            based in Portugal.
+            I&apos;m a software engineer with a bioinformatics background, based
+            in Portugal.
           </p>
         </div>
         <div>
-          <button>Contact me</button>
+          <button className="font-mono text-orange-400">Contact me</button>
         </div>
       </div>
     </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      gray: colors.coolGray,
+      white: colors.white,
+      orange: colors.amber,
+      purple: {
+        lightest: '#D6C1EB',
+        light: '#C2A3E1',
+        DEFAULT: '#84719B',
+        dark: '#2A1541',
+        darkest: '#1E0F2E',
+      },
+    },
+    fontFamily: {
+      mono: ['Fira Code', 'monospace'],
+    },
     extend: {},
   },
   variants: {
